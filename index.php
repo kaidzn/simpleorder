95b7ffffb8ffdbe05b6501f07554e4bc505db56a
kaidzn <83287458+kaidzn@users.noreply.github.com>
2024-06-26 08:05:34 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="index.php" method="post">
-        <label>Enter a number to count to:</label>
-        <input type="text" name="counter">
-        <input type="submit" value="start">
-    </form>
-</body>
-</html>
 <?php
 
-    $counter = $_POST["counter"];
+    class Chef{
 
-    for($i = 1; $i <= $counter; $i++){
-        echo $i . "<br>";
+        function makeChicken(){
+            echo "The chef makes chicken <br>";
+        }
+
+        function makeSalad(){
+            echo "The chef makes salad <br>";
+        }
+
+        function makeSpecialDish(){
+            echo "The chef makes bbq ribs <br>";
+        }
+
+
     }
+
+    class ItalianChef extends Chef{
+        function makePasta(){
+            echo "The chef makes pasta";
+        }
+        function makeSpecialDish()
+        {
+            echo "The chef makes chicken parm";
+        }
+    }
+
+    $chef = new Chef();
+    $chef->makeSpecialDish();
+
+    $italianChef = new ItalianChef();
+    $italianChef->makeSpecialDish();
 
 
 ?>
